d2132eb001308ee4c1fbc2b77afc451f4bd8a444
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-05-07 17:13:46 -0400

    Break ephemeral port Ansible role out into its own repo

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,7 +10,27 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+@pytest.mark.parametrize("directory", [{"path": "/etc/sysctl.d", "mode": "0o755"}])
+def test_directories(host, directory):
+    """Test that the appropriate directories were created."""
+    assert host.file(directory["path"]).exists
+    assert host.file(directory["path"]).is_directory
+    if "mode" in directory:
+        assert oct(host.file(directory["path"]).mode) == directory["mode"]
+
+
+@pytest.mark.parametrize(
+    "file,content",
+    [
+        (
+            "/etc/sysctl.d/99-more_ephemeral_ports.conf",
+            "^net.ipv4.ip_local_port_range = 1024 65535$",
+        )
+    ],
+)
+def test_files(host, file, content):
+    """Test that config files were modified as expected."""
+    f = host.file(file)
+
+    assert f.exists
+    assert f.contains(content)
